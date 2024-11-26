9222ef6a54fd8505bf75408cea72d6e9b5b8f292
3412ysw <3412ysw@gmail.com>
2024-11-26 17:10:16 +0900

    [ysw] 변수선언

---
+++ lec02_variable/src/com/gn/practice/Practice01.java
@@ -0,0 +1,13 @@
+package com.gn.practice;
+
+public class Practice01 {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 10;
+		System.out.println(a+b);
+		System.out.println(b *(a+b));
+		System.out.println((b*(a+b))/b);
+	//변수 사용 시 유지보수 효율적
+		
+	}
+}
--- lec02_variable/src/com/gn/study/VariableBasic.java
+++ lec02_variable/src/com/gn/study/VariableBasic.java
@@ -3,6 +3,19 @@ package com.gn.study;
 public class VariableBasic {
 	// 컨트롤+스페이스 -> 메인
 	public static void main(String[] args) {
-				
+		//1. 변수 선언
+		int numder;
+		//2. 변수의 초기화
+		int age;
+		age = 100;
+		int age2 = 100;
+		//3. 변수 표준출력
+		System.out.println(age);
+		//4. 변수의 재할당
+		int score = 90;
+		System.out.println("1 : "+score);
+		score = 95;
+		System.out.println("2 : "+score);
+		
 	}
 }
