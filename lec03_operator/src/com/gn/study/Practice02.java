2778c595030e3534df7dc9bc20ba89ffbe8e6df0
3412ysw <3412ysw@gmail.com>
2024-11-27 15:28:44 +0900

    [ysw] operator

---
+++ lec03_operator/src/com/gn/study/Practice02.java
@@ -0,0 +1,20 @@
+package com.gn.study;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		
+		int result = 100;
+		result += 3;
+		System.out.println(result); //103
+		result -= 10;
+		System.out.println(result); //93
+		result *= 2;
+		System.out.println(result); //186
+		result /= 3;
+		System.out.println(result); //62
+		result %= 30;
+		System.out.println(result); //2
+	}
+
+}
