7d1bf1ef9633987aa7f04ed14daa7c13577bef6e
3412ysw <3412ysw@gmail.com>
2024-12-02 11:01:22 +0900

    [ysw] 12/02 (1)

---
+++ lec04_control/src/com/gn/study/Practice10.java
@@ -0,0 +1,19 @@
+package com.gn.study;
+
+import java.util.Scanner;
+
+public class Practice10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 : ");
+		int su = sc.nextInt();
+		do {
+			System.out.println(su);
+			su++;
+		}while(su<=10);
+		
+		
+	}
+
+}
