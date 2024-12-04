e6bbb9e9e13e9b9ccf626e6d02f2597e549a1cde
3412ysw <3412ysw@gmail.com>
2024-12-04 19:52:25 +0900

    [ysw] 12/04(2)

--- lec05_array/src/com/gn/homework/dimensional/Practice.java
+++ lec05_array/src/com/gn/homework/dimensional/Practice.java
@@ -43,13 +43,94 @@ public class Practice {
 		}
 		System.out.print("\n");
 		System.out.print("총합 : "+ total);
-		
+	sc.close();	
 	}
 	
 	public void practice03() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 : ");
+		int enter1 = sc.nextInt();
+		while(true) {
+			if(enter1%2==0 || enter1 < 3) {
+				System.out.println("다시 입력하세요.");
+				System.out.print("정수 : ");
+				enter1 = sc.nextInt();
+			} else {
+				int[] arr = new int[enter1];
+				int num = 0;
+		    	for(int i=0; i<enter1; i++) {
+		    		if(i>enter1/2) {
+		    			num--;
+		    		} else {
+		    			num++;
+		    		}
+		    		arr[i] = num;
+		    	}
+		    	for(int i=0; i<arr.length; i++) {
+					System.out.print(arr[i]);
+					if(i==arr.length-1) {
+						continue;
+					}
+					System.out.print(", ");
+				}
+		    	break;
+			}
+		}
 		
+		
+	}	
+		
+		
+
+	
+	public void practice04() {
+		
+		for(int i = 0 ; i < 6 ; i++) {
+		int rnd1 = (int)(Math.random()*45)+1;
+		   
+			}
+		
+		}
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void practice05() {
+	
 	}
 	
+	
+	
+	public void practice06() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("배열의 크기를 입력하세요 : ");
+		int number = sc.nextInt();
+		for( int i=1 ; i <=)
+		//int[] arrnum = new int[number];
+		
+		
+		
+		
+		
+	
+	}
+	
+	
+	
+	public void practice07() {
+	
+	}
+	public void practice08() {
+		
+	}
 	
 	
 	
--- lec05_array/src/com/gn/homework/dimensional/Run.java
+++ lec05_array/src/com/gn/homework/dimensional/Run.java
@@ -9,7 +9,11 @@ public class Run {
 		System.out.println("1. 키순서대로 앉으세요.");
 		System.out.println("2. 값 입력 받아 배열 만들기");
 		System.out.println("3. 올라갔다 내려갔다");
+		System.out.println("4. 로또 번호 자동 생성기");
 		System.out.println("5. 컴퓨터와 가위바위보");
+		System.out.println("6. 배열 늘리기");
+		System.out.println("7. 문서 복사하기");
+		System.out.println("8. 오늘의 메뉴판 만들기");
 		System.out.print("선택 : ");
 		
 		Scanner sc = new Scanner(System.in);
@@ -23,8 +27,16 @@ public class Run {
 				break;
 			case 3 : practice.practice03();
 			    break;
-//			case 5 : practice.practice05();
-//				break;
+			case 4 : practice.practice04();
+				break;
+			case 5 : practice.practice05();
+				break;
+			case 6 : practice.practice06();
+		    	break;
+			case 7 : practice.practice07();
+				break;
+			case 8 : practice.practice08();
+		    	break;
 		}
 		sc.close();
         
